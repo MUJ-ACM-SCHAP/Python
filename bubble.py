d493b511b48a45faaad73a5cecddc0eb2b859cca
Sagarikac310 <43610681+Sagarikac310@users.noreply.github.com>
2018-10-26 20:55:53 +0530

    Create bubble.py

---
+++ bubble.py
@@ -0,0 +1,11 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [2,31,88,99,58,7,360,81,96,15]
+bubbleSort(alist)
+print(alist)
