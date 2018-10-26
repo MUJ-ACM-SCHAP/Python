92d016012c7e0f4948ab56025e6c47e44d76d5af
Sagarikac310 <43610681+Sagarikac310@users.noreply.github.com>
2018-10-26 20:58:23 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,10 @@
+
+# Python code to compute factorial 
+n = int(input("Enter a number: "))
+fact = 1
+  
+for i in range(1,n+1): 
+    fact = fact * i 
+      
+print ("The factorial is : ") 
+print (fact) 
